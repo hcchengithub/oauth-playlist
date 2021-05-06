227f564f5f5c2144d2e787e4fe5bcebfe2e46d35
HCChen <h.c._chen@wistron.com>
2021-05-06 13:46:02 +0800

    my notes

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express');
 
 const app = express();
 
-// set view engine
-app.set('view engine', 'ejs');
+// set view engine --> what is 'view engine' https://stackoverflow.com/questions/8308485/what-is-view-engine-what-does-it-actually-do
+app.set('view engine', 'ejs');  // ejs or many others. 
 
 // create home route
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home');  // home.ejs because we've specified the 'view engine' as so.  
 });
 
 app.listen(3000, () => {
+    // This is the call back function to know when the listening would have been really started 
     console.log('app now listening for requests on port 3000');
 });
